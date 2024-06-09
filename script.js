23f6d2af1b228d0336d6ffa0ed15f12b7bf3cf2e
ENMGONDEH <147666147+ENMGONDEH@users.noreply.github.com>
2024-06-09 21:12:54 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let clickRate = parseInt(localStorage.getItem('clickRate')) || 1;
 let clickLimit = parseInt(localStorage.getItem('clickLimit')) || 5;
 let clickValue = parseInt(localStorage.getItem('clickValue')) || 1;
 let upgradeCost = parseInt(localStorage.getItem('upgradeCost')) || 100;
+let upgradeValueCost = parseInt(localStorage.getItem('upgradeValueCost')) || 100;
 let clicks = parseInt(localStorage.getItem('clicks')) || 0;
 let lastClickTime = Date.now();
 let upgradeLevel = parseInt(localStorage.getItem('upgradeLevel')) || 1;
@@ -12,6 +13,7 @@ document.getElementById('coins').innerText = coins;
 document.getElementById('clickRate').innerText = clickRate;
 document.getElementById('clickValue').innerText = clickValue;
 document.getElementById('upgradeCost').innerText = upgradeCost;
+document.getElementById('upgradeValueCost').innerText = upgradeValueCost;
 document.getElementById('clicks').innerText = clicks;
 document.getElementById('upgradeLevel').innerText = upgradeLevel;
 
@@ -21,6 +23,7 @@ function saveState() {
     localStorage.setItem('clickLimit', clickLimit);
     localStorage.setItem('clickValue', clickValue);
     localStorage.setItem('upgradeCost', upgradeCost);
+    localStorage.setItem('upgradeValueCost', upgradeValueCost);
     localStorage.setItem('clicks', clicks);
     localStorage.setItem('upgradeLevel', upgradeLevel);
 }
@@ -53,14 +56,14 @@ function upgradeClickValue() {
         return;
     }
     
-    if (coins >= upgradeCost) {
-        coins -= upgradeCost;
+    if (coins >= upgradeValueCost) {
+        coins -= upgradeValueCost;
         clickValue++;
-        upgradeCost *= 2;
+        upgradeValueCost *= 2;
         upgradeLevel++;
         document.getElementById("coins").innerText = coins;
         document.getElementById("clickValue").innerText = clickValue;
-        document.getElementById("upgradeCost").innerText = upgradeCost;
+        document.getElementById("upgradeValueCost").innerText = upgradeValueCost;
         document.getElementById("upgradeLevel").innerText = upgradeLevel;
         saveState();
     } else {
